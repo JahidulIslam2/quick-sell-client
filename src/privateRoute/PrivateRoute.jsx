68d811d32feda0516953f7019b222eb6e43e121f
JahidulIslam2 <jahid2111@gmail.com>
2022-11-26 21:00:18 +0600

    add private route

--- src/privateRoute/PrivateRoute.jsx
+++ src/privateRoute/PrivateRoute.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { useContext } from 'react';
 import { ContextProvider } from '../components/authContext/AuthContext';
+import { Navigate, useLocation } from 'react-router-dom';
+import Loader from '../shared/loader/Loader';
 
-const PrivateRoute = () => {
+const PrivateRoute = ({children}) => {
 
     const {user,loader} =useContext(ContextProvider)
+    const location=useLocation();
+
+
+    if(loader){
+        return <Loader></Loader>
+    }
+
+
+    if(!user){
+      return <Navigate to='/Login' state={{from:location}} replace/>
+    }
+
+
+    return children;
 
     
 
